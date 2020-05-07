4e8f7e8a60d7910ab254218a476c82213bc72beb
Andrew Aldridge <i80and@foxquill.com>
2020-05-07 12:37:53 -0400

    Initial commit

---
+++ src/Query.ts
@@ -0,0 +1,273 @@
+'use strict'
+
+const CORRELATIONS = [
+    ['regexp', 'regex', 0.8],
+    ['regular expression', 'regex', 0.8],
+    ['ip', 'address', 0.1],
+    ['address', 'ip', 0.1],
+    ['join', 'lookup', 0.6],
+    ['join', 'sql', 0.25],
+    ['aggregation', 'sql', 0.1],
+    ['aggregation', 'pipeline', 0.1],
+    ['least', 'min', 0.6],
+    ['set security', 'keyfile', 1.0],
+    ['cluster security', 'keyfile', 1.0],
+    ['x509', 'x.509', 1.0],
+    ['auth', 'authentication', 0.25]]
+
+const stopWords = new Set([
+    'a',
+    'able',
+    'about',
+    'across',
+    'after',
+    'all',
+    'almost',
+    'also',
+    'am',
+    'among',
+    'an',
+    'and',
+    'any',
+    'are',
+    'as',
+    'at',
+    'be',
+    'because',
+    'been',
+    'but',
+    'by',
+    'can',
+    'cannot',
+    'could',
+    'dear',
+    'did',
+    'do',
+    'does',
+    'either',
+    'else',
+    'ever',
+    'every',
+    'for',
+    'from',
+    'got',
+    'had',
+    'has',
+    'have',
+    'he',
+    'her',
+    'hers',
+    'him',
+    'his',
+    'how',
+    'however',
+    'i',
+    'i.e.',
+    'if',
+    'important',
+    'in',
+    'into',
+    'is',
+    'it',
+    'its',
+    'just',
+    'may',
+    'me',
+    'might',
+    'most',
+    'must',
+    'my',
+    'neither',
+    'no',
+    'nor',
+    'of',
+    'off',
+    'often',
+    'on',
+    'only',
+    'or',
+    'other',
+    'our',
+    'own',
+    'rather',
+    'said',
+    'say',
+    'says',
+    'she',
+    'should',
+    'since',
+    'so',
+    'some',
+    'than',
+    'that',
+    'the',
+    'their',
+    'them',
+    'then',
+    'there',
+    'these',
+    'they',
+    'this',
+    'tis',
+    'to',
+    'too',
+    'twas',
+    'us',
+    'wants',
+    'was',
+    'we',
+    'were',
+    'what',
+    'where',
+    'which',
+    'while',
+    'who',
+    'whom',
+    'why',
+    'will',
+    'with',
+    'would',
+    'yet',
+    'you',
+    'your',
+    'e.g.'])
+
+const atomicPhraseMap: Record<string, string> = {
+    'ops': 'manager',
+    'cloud': 'manager',
+    'real': 'time'
+}
+const atomicPhrases = new Set(Object.entries(atomicPhraseMap).map((kv) => kv.join(' ')))
+
+const wordCache = new Map()
+
+function isStopWord(word: string): boolean {
+    return stopWords.has(word)
+}
+
+function tokenize(text: string, fuzzy: boolean): string[] {
+    const components = text.split(/[^\w$%.]+/).map((token) => {
+        return token.toLocaleLowerCase().replace(/(?:^\.)|(?:\.$)/g, '')
+    })
+
+    const tokens = []
+    for (let i = 0; i < components.length; i += 1) {
+        const token = components[i]
+
+        if (token == '$') {
+            tokens.push('positional')
+            tokens.push('operator')
+            continue
+        }
+
+        const nextToken = components[i + 1]
+        if (nextToken !== undefined && atomicPhraseMap[token] === nextToken) {
+            i += 1
+            tokens.push(`${token} ${atomicPhraseMap[token]}`)
+            continue
+        }
+
+        if (token.length > 1) {
+            tokens.push(token)
+        }
+
+        const subtokens = token.split('.')
+        if (fuzzy && subtokens.length > 1) {
+            for (const subtoken of subtokens) {
+                if (subtoken.length > 1) {
+                    tokens.push(subtoken)
+                }
+            }
+        }
+    }
+
+    return tokens
+}
+
+function processPart(part: string): string[] {
+    return tokenize(part, false)
+}
+
+/** A parsed search query. */
+export class Query {
+    terms: Set<string>
+    phrases: string[]
+    rawQuery: string
+
+    /**
+     * Create a new query.
+     * @param {string} queryString The query to parse
+     */
+    constructor(queryString: string) {
+        this.terms = new Set()
+        this.phrases = []
+        this.rawQuery = queryString
+
+        const parts = queryString.split(/((?:\s+|^)"[^"]+"(?:\s+|$))/)
+        let inQuotes = false
+        for (const part of parts) {
+            inQuotes = Boolean(part.match(/^\s*"/))
+
+            if (!inQuotes) {
+                this.addTerms(processPart(part))
+            } else {
+                const phraseMatch = part.match(/\s*"([^"]*)"?\s*/)
+                if (!phraseMatch) {
+                    // This is a phrase fragment
+                    this.addTerms(processPart(part))
+                    continue
+                }
+
+                const phrase = phraseMatch[1].toLowerCase().trim()
+                this.phrases.push(phrase)
+
+                const phraseParts = processPart(phrase)
+                this.addTerms(phraseParts)
+            }
+        }
+    }
+
+    addTerms(terms: string[]) {
+        for (const term of terms) {
+            this.terms.add(term)
+        }
+    }
+
+    getAggregationQuery(searchProperty: string | null): any[] {
+        const parts: any[] = []
+        const terms = Array.from(this.terms)
+
+        parts.push({text: {
+            query: terms,
+            path: "text"
+        }})
+
+        parts.push({text: {
+            query: terms,
+            path: "headings",
+            score: { boost: { value: 5 } }
+        }})
+
+        parts.push({text: {
+            query: terms,
+            path: "title",
+            score: { boost: { value: 10 } }
+        }})
+
+        parts.push({text: {
+            query: terms,
+            path: "tags",
+            score: { boost: { value: 10 } }
+        }})
+
+        const filter = (searchProperty !== null) ? {searchProperty: searchProperty} : {includeInGlobalSearch: true}
+
+        return [{
+            $searchBeta: {
+                compound: {
+                    should: parts
+                }
+            }
+        }, {$match: filter}]
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,511 @@
+#!/usr/bin/env node
+'use strict'
+
+import { MongoClient, Collection, TransactionOptions, BulkWriteOperation, Db } from "mongodb"
+import assert from 'assert'
+import crypto from 'crypto'
+import fs from 'fs'
+import http from 'http'
+import {parse as parseUrl, UrlWithParsedQuery} from 'url'
+import util from 'util'
+
+// @ts-ignore
+import dive from 'dive'
+// @ts-ignore
+import Logger from 'basic-logger'
+
+import S3 from 'aws-sdk/clients/s3'
+import {Query} from "./Query"
+
+process.title = 'search-transport'
+
+const MAXIMUM_QUERY_LENGTH = 100
+
+const STANDARD_HEADERS = {
+    'X-Content-Type-Options': 'nosniff'
+}
+
+const log = new Logger({
+    showTimestamp: true,
+})
+
+interface RefreshInfo {
+    deleted: number | null
+    updated: string[]
+    skipped: string[]
+    errors: Error[]
+    dateStarted: Date
+    dateFinished: Date | null
+    elapsedMS: number | null
+}
+
+interface Document {
+    slug: string
+    title: string
+    headings: string[]
+    text: string
+    preview: string
+    tags: string
+    links: string[]
+}
+
+interface ManifestData {
+    documents: Document[]
+    includeInGlobalSearch: boolean
+    url: string
+    aliases: string[]
+}
+
+interface Manifest {
+    manifest: ManifestData
+    lastModified: Date
+    manifestRevisionId: string
+    searchProperty: string
+}
+
+interface DatabaseDocument extends Document {
+    manifestRevisionId: string
+    searchProperty: string
+    includeInGlobalSearch: boolean
+}
+
+function generateHash(data: string): Promise<string> {
+    const hash = crypto.createHash('sha256')
+
+    return new Promise((resolve, reject) => {
+        hash.on('readable', () => {
+            const data = hash.read();
+            if (data) {
+                resolve(data.toString('hex'))
+            }
+        });
+
+        hash.write(data);
+        hash.end();
+    })
+}
+
+/**
+ * If the request method does not match the method parameter, return false
+ * and write a 405 status code. Otherwise return true.
+ */
+function checkMethod(req: http.IncomingMessage, res: http.ServerResponse, method: string): boolean {
+    if (req.method !== method) {
+        res.writeHead(405, {})
+        res.end('')
+        return false
+    }
+
+    return true
+}
+
+class Index {
+    currentlyIndexing?: boolean
+    manifestSource: string
+    manifests: Manifest[]
+    errors: any[]
+    lastSyncDate: Date | null
+
+    constructor(manifestSource: string) {
+        this.manifestSource = manifestSource
+        this.manifests = []
+        this.errors = []
+
+        this.lastSyncDate = null
+    }
+
+    getStatus() {
+        return {
+            manifests: this.manifests,
+            lastSync: {
+                errors: this.errors,
+                finished: this.lastSyncDate ? this.lastSyncDate.toISOString() : null
+            }
+        }
+    }
+
+    async getManifestsFromS3(bucketName: string, prefix: string): Promise<Manifest[]> {
+        const s3 = new S3({apiVersion: '2006-03-01'})
+        const result = await util.promisify(s3.makeUnauthenticatedRequest.bind(s3))('listObjectsV2', {
+            Bucket: bucketName,
+            Prefix: prefix
+        })
+
+        if (result.IsTruncated) {
+            // This would indicate something awry, since we shouldn't
+            // ever have more than 1000 properties. And if we ever did,
+            // everything would need to be rearchitected.
+            throw new Error('Got truncated response from S3')
+        }
+
+        const manifests = []
+        for (const bucketEntry of result.Contents) {
+            if (bucketEntry.Size === 0) {
+                continue
+            }
+
+            const matches = bucketEntry.Key.match(/([^/]+).json$/)
+            if (matches === null) {
+                this.errors.push(`Got weird filename in manifest listing: "${bucketEntry.Key}"`)
+                continue
+            }
+
+            const searchProperty = matches[1]
+            const data = await util.promisify(s3.makeUnauthenticatedRequest.bind(s3))('getObject', {
+                Bucket: bucketName,
+                Key: bucketEntry.Key
+            })
+
+            const body = data.Body.toString('utf-8')
+            const hash = await generateHash(body)
+            const parsed = JSON.parse(body)
+            manifests.push({
+                manifest: parsed,
+                lastModified: data.LastModified,
+                manifestRevisionId: hash,
+                searchProperty: searchProperty
+            })
+        }
+
+        return manifests
+    }
+
+    getManifestsFromDirectory(prefix: string): Promise<Manifest[]> {
+        return new Promise((resolve, reject) => {
+            const manifests: Manifest[] = []
+
+            dive(prefix, async (err: Error | null, path: string, stats: fs.Stats) => {
+                if (err) { reject(err) }
+                const matches = path.match(/([^/]+).json$/)
+                if (!matches) { return }
+                const searchProperty = matches[1]
+                const data = fs.readFileSync(path, {encoding: 'utf-8'})
+                const parsed = JSON.parse(data)
+                const hash = await generateHash(data)
+
+                manifests.push({
+                    manifest: parsed,
+                    lastModified: stats.mtime,
+                    manifestRevisionId: hash,
+                    searchProperty: searchProperty
+                })
+            }, () => {
+                resolve(manifests)
+            })})
+    }
+
+    async getManifests() {
+        const parsedSource = this.manifestSource.match(/((?:bucket)|(?:dir)):(.+)/)
+        if (!parsedSource) {
+            throw new Error('Bad manifest source')
+        }
+
+        let manifests
+        if (parsedSource[1] === 'bucket') {
+            const parts = parsedSource[2].split('/', 2)
+            const bucketName = parts[0].trim()
+            const prefix = parts[1].trim()
+            if (!bucketName.length || !prefix.length) {
+                throw new Error('Bad bucket manifest source')
+            }
+            manifests = await this.getManifestsFromS3(bucketName, prefix)
+        } else if (parsedSource[1] === 'dir') {
+            manifests = await this.getManifestsFromDirectory(parsedSource[2])
+        } else {
+            throw new Error('Unknown manifest source protocol')
+        }
+
+        return manifests
+    }
+
+    async load() {
+        if (this.currentlyIndexing) {
+            throw new Error('already-indexing')
+        }
+        this.currentlyIndexing = true
+
+        try {
+            this.manifests = await this.getManifests()
+        } catch (err) {
+            throw err
+        } finally {
+            this.currentlyIndexing = false
+        }
+
+        this.errors = []
+    }
+}
+
+class Marian {
+    index: Index
+    client: MongoClient
+    db: Db
+    collection: Collection
+
+    constructor(bucket: string, client: MongoClient) {
+        this.index = new Index(bucket)
+        this.client = client
+
+        this.db = client.db('search')
+        this.collection = this.db.collection('documents')
+
+        // Fire-and-forget loading
+        this.refresh().then(result => {
+            log.info(JSON.stringify(result))
+        }).catch((err) => {
+            log.error(err)
+        })
+    }
+
+    start(port: number) {
+        const server = http.createServer(async (req, res) => {
+            try {
+                await this.handle(req, res)
+            } catch(err) {
+                log.error(err)
+                res.writeHead(500, {})
+                res.end('')
+            }
+        })
+
+        server.listen(port, () => {
+            log.info(`Listening on port ${port}`)
+        })
+    }
+
+    handle(req: http.IncomingMessage, res: http.ServerResponse): void {
+        const url = req.url
+        if (!url) {
+            assert.fail("Assertion: Missing url")
+        }
+        const parsedUrl = parseUrl(url, true)
+
+        const pathname = (parsedUrl.pathname || "").replace(/\/+$/, '')
+        assert.ok(pathname)
+
+        if (pathname === '/search') {
+            if (checkMethod(req, res, 'GET')) {
+                this.handleSearch(parsedUrl, req, res)
+            }
+        } else if (pathname === '/refresh') {
+            if (checkMethod(req, res, 'POST')) {
+                this.handleRefresh(req, res)
+            }
+        } else if (pathname === '/status') {
+            if (checkMethod(req, res, 'GET')) {
+                this.handleStatus(req, res)
+            }
+        } else {
+            res.writeHead(400, {})
+            res.end('')
+        }
+    }
+
+    async refresh(): Promise<RefreshInfo> {
+        const session = this.client.startSession()
+        const transactionOptions: TransactionOptions = {
+            readPreference: 'primary',
+            readConcern: { level: 'local' },
+            writeConcern: { w: 'majority' }
+        };
+
+        const startTime = process.hrtime.bigint()
+        const status: RefreshInfo = {
+            deleted: 0,
+            updated: [],
+            skipped: [],
+            errors: [],
+            dateStarted: new Date(),
+            dateFinished: null,
+            elapsedMS: null
+        }
+
+        try {
+            log.info("Starting fetch")
+            await this.index.load()
+            log.info(`Finished fetch: ${this.index.manifests.length} entries`)
+
+            await this.db.createCollection("documents")
+
+            for (const manifest of this.index.manifests) {
+                assert.equal(typeof manifest.searchProperty, "string")
+                assert.ok(manifest.searchProperty)
+                assert.equal(typeof manifest.manifestRevisionId, "string")
+                assert.ok(manifest.manifestRevisionId)
+
+                log.debug(`Starting transaction: ${manifest.searchProperty}`)
+                await session.withTransaction(async () => {
+                    // const countResult = await this.collection.estimatedDocumentCount({
+                    //     manifestRevisionId: manifest.manifestRevisionId
+                    // })
+                    // if (countResult > 0) {
+                    //     status.skipped.push(manifest.searchProperty)
+                    //     return
+                    // }
+
+                    // await this.collection.deleteMany({
+                    //     searchProperty: manifest.searchProperty,
+                    // }, {session})
+
+                    const operations: BulkWriteOperation<DatabaseDocument>[] = manifest.manifest.documents.map((document) => {
+                        assert.equal(typeof document.slug, "string")
+                        assert.ok(document.slug)
+
+                        const newDocument: DatabaseDocument = {
+                            ...document,
+                            manifestRevisionId: manifest.manifestRevisionId,
+                            searchProperty: manifest.searchProperty,
+                            includeInGlobalSearch: manifest.manifest.includeInGlobalSearch,
+                        }
+
+                        return {
+                            updateOne: {
+                                filter: {searchProperty: newDocument.searchProperty, slug: newDocument.slug},
+                                update: {$set: newDocument},
+                                upsert: true
+                            }
+                        }
+                    })
+
+                    await this.collection.bulkWrite(operations, {session, ordered: false})
+                }, transactionOptions)
+
+                status.updated.push(manifest.searchProperty)
+            }
+
+            log.info("Deleting old properties")
+            const deleteResult = await this.collection.deleteMany(
+                {
+                    searchProperty: {
+                        $nin: this.index.manifests.map(manifest => manifest.searchProperty)
+                    }
+                },
+                {session, w: "majority"})
+            status.deleted = (deleteResult.deletedCount === undefined) ? null : deleteResult.deletedCount
+        } catch(err) {
+            log.error()
+            status.errors.push(err)
+        } finally {
+            session.endSession()
+            log.info("Done!")
+        }
+
+        status.dateFinished = new Date()
+        status.elapsedMS = Number(process.hrtime.bigint() - startTime) / 1000000
+        return status
+    }
+
+    async handleSearch(parsedUrl: UrlWithParsedQuery, req: http.IncomingMessage, res: http.ServerResponse): Promise<void> {
+        const start = process.hrtime.bigint()
+        const headers = {
+            'Content-Type': 'application/json',
+            'Vary': 'Accept-Encoding',
+            'Cache-Control': 'public,max-age=120,must-revalidate',
+            'Access-Control-Allow-Origin': '*',
+        }
+        Object.assign(headers, STANDARD_HEADERS)
+
+        const rawQuery = (parsedUrl.query.q || "").toString()
+        if (!rawQuery) {
+            res.writeHead(400, headers)
+            res.end('[]')
+            return
+        }
+
+        if (rawQuery.length > MAXIMUM_QUERY_LENGTH) {
+            res.writeHead(400, headers)
+            res.end('[]')
+            return
+        }
+
+        const query = new Query(rawQuery)
+
+        const aggregationQuery = query.getAggregationQuery((parsedUrl.query.searchProperty || "").toString())
+        log.info(JSON.stringify(aggregationQuery, null, 4))
+        aggregationQuery.push({$limit: 50})
+        aggregationQuery.push({$project: {
+            "_id": 0,
+            "title": 1,
+            "preview": 1
+        }})
+        const cursor = await this.collection.aggregate(aggregationQuery)
+
+        const results = await cursor.toArray()
+        let responseBody = JSON.stringify(results)
+        res.writeHead(200, headers)
+        res.end(responseBody)
+
+        const end = process.hrtime.bigint()
+        log.info(Number(end - start) / 1000000)
+    }
+
+    async handleRefresh(req: http.IncomingMessage, res: http.ServerResponse): Promise<void> {
+        const headers: Record<string, string> = {
+            'Vary': 'Accept-Encoding'
+        }
+        Object.assign(headers, STANDARD_HEADERS)
+
+        try {
+            log.info(JSON.stringify(await this.refresh()))
+        } catch(err) {
+            log.error(err)
+            headers['Content-Type'] = 'application/json'
+            const body = JSON.stringify({'errors': [err]})
+
+            if (err.message === 'already-indexing') {
+                res.writeHead(503, headers)
+            } else {
+                res.writeHead(500, headers)
+            }
+            res.end(body)
+            return
+        }
+
+        if (this.index.errors.length > 0) {
+            headers['Content-Type'] = 'application/json'
+            const body = JSON.stringify({'errors': this.index.errors})
+            res.writeHead(200, headers)
+            res.end(body)
+            return
+        }
+
+        res.writeHead(200, headers)
+        res.end('')
+    }
+
+    async handleStatus(req: http.IncomingMessage, res: http.ServerResponse): Promise<void> {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Vary': 'Accept-Encoding',
+            'Pragma': 'no-cache'
+        }
+        Object.assign(headers, STANDARD_HEADERS)
+
+        res.writeHead(200, headers)
+        res.end()
+    }
+}
+
+async function main() {
+    Logger.setLevel('info', true)
+
+    if (process.argv.length != 4) {
+        console.error("Usage: search-transport <manifest-uri> <mongodb-uri>")
+        process.exit(1)
+    }
+
+    const client = new MongoClient(process.argv[3])
+    client.connect((err) => {
+        assert.equal(null, err)
+        log.info('Connected correctly to MongoDB')
+
+        const server = new Marian(process.argv[2], client)
+        server.start(8080)
+    })
+}
+
+try {
+    main()
+} catch (err) {
+    console.error(err)
+}
