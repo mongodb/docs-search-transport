ede2a9bfafc1cefcb6c2e25476c559c3e1dcd788
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-05-18 20:40:01 -0600

    Ignore build dir, build in Docker, add src to allowlist

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM node:14-alpine
 RUN mkdir -p /app
 WORKDIR /app
-COPY . /app
+COPY . .
 
 ENV NODE_ENV=production
-RUN npm ci --production && chown -R node:node /app
+RUN npm ci --ignore-scripts && npm run build:clean
+RUN chown -R node:node /app
 USER node
 
 EXPOSE 8080
---
+++ build/SearchHeuristics/index.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.applyHeuristics = void 0;
+const hasMinimumTextThreshold = (doc) => {
+    const MINIMUM_CONTENT_LENGTH = 40;
+    return doc.text.length >= MINIMUM_CONTENT_LENGTH;
+};
+const hasPreview = (doc) => !!doc.preview?.trim();
+const isInteractive = (doc) => doc.links?.length >= 2;
+const meetsMinimumSearchableStandard = (doc) => {
+    const failsThreshold = !hasPreview(doc) && (!hasMinimumTextThreshold(doc) || !isInteractive(doc));
+    return !failsThreshold;
+};
+const applyHeuristics = (documents) => {
+    const filteredDocuments = {
+        searchable: [],
+        unsearchable: [],
+    };
+    documents.forEach((doc) => {
+        meetsMinimumSearchableStandard(doc)
+            ? filteredDocuments.searchable.push(doc)
+            : filteredDocuments.unsearchable.push(doc);
+    });
+    return filteredDocuments;
+};
+exports.applyHeuristics = applyHeuristics;
