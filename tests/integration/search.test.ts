f6b4be48c9a4a592d9cffc3226777213e36d6293
Cassidy Schaufele <cassidyschaufele@gmail.com>
2023-05-22 22:53:10 -0600

    Simplify url correction test

--- tests/integration/search.test.ts
+++ tests/integration/search.test.ts
@@ -1,4 +1,4 @@
-import { strictEqual, deepStrictEqual, ok } from 'assert';
+import { strictEqual } from 'assert';
 import { Query } from '../../src/Query';
 import { SearchIndex } from '../../src/SearchIndex';
 import { MongoClient } from 'mongodb';
@@ -32,15 +32,7 @@ describe('Searching', function () {
   // Test variants of searchProperty
   it('should properly handle incorrect urls in manifests', async () => {
     let result = await index.search(new Query('manual'), ['manual-v5.1']);
-
-    deepStrictEqual(result, [
-      {
-        preview:
-          'MongoDB 5.1 release candidates are not yet available. This version of the manual is for an upcoming release and is currently a work in progress.',
-        title: 'The MongoDB 5.1 Manual (Upcoming Release) — MongoDB Manual',
-        url: 'https://docs.mongodb.com/v5.1/index.html',
-      },
-    ]);
+    strictEqual(result[0].url, 'https://docs.mongodb.com/v5.1/index.html');
   });
 
   after(async function () {
