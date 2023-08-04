f3255ed248dc1a91ffb34c7eb77f2a12ed4db771
Seung Park <seung.park@mongodb.com>
2023-08-04 15:24:25 -0400

    no mutate. copy over values

--- src/AtlasAdmin.ts
+++ src/AtlasAdmin.ts
@@ -135,12 +135,12 @@ export class AtlasAdminManager {
 
   private getPostPatchOptions(searchIndex: IndexMappings, taxonomy: Taxonomy) {
     const options = { ...DEFAULT_ATLAS_API_OPTIONS };
-    options['data'] = this.insertTaxonomyIntoSearchIndex(searchIndex, taxonomy);
-    options['data'] = Object.assign(options['data'], {
+    options['data'] = {
+      ...this.insertTaxonomyIntoSearchIndex(searchIndex, taxonomy),
       collectionName: COLLECTION_NAME,
       database: DB,
       name: SEARCH_INDEX,
-    });
+    };
     return options;
   }
 
