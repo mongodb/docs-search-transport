31948dea2e4184175ee0bf5f6625a90be920c455
Seung Park <seung.park@mongodb.com>
2023-09-26 16:05:58 -0400

    minor. rename trie to list

--- tests/unit/SearchIndex.test.ts
+++ tests/unit/SearchIndex.test.ts
@@ -1,6 +1,6 @@
 import { strictEqual, deepStrictEqual } from 'assert';
 import { joinUrl, convertTaxonomyResponse } from '../../src/SearchIndex/util';
-import { sampleFacetTrie, sampleTaxonomy } from '../resources/utils-data';
+import { sampleFacetOption, sampleTaxonomy } from '../resources/utils-data';
 
 describe('SearchIndex', function () {
   it('correctly joins base URLs with slugs', function () {
@@ -11,7 +11,7 @@ describe('SearchIndex', function () {
   describe('convertTaxonomyResponse', () => {
     it('converts taxonomy object into a trie structure', () => {
       const input = sampleTaxonomy;
-      const expected = sampleFacetTrie;
+      const expected = sampleFacetOption;
       deepStrictEqual(convertTaxonomyResponse(input), expected);
     });
   });
