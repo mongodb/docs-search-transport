c498063a71bae5a5a4e90b498b2976a6fd2b111c
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2023-10-04 14:05:55 -0400

    (Tests for) DOP-4057: Add frontend S3 staging site to Docs Search Transport allowed origins (#79)
    * DOP-4057 changed allowed origins
    
    * DOP-4057 prettier
    
    * DOP-4057 drone yaml
    
    * DOP-4057 testing for staging search origin permission
    
    * DOP-4057 Prettier
    
    * DOP-4057 remove branch from drone yml

--- tests/integration/server.test.ts
+++ tests/integration/server.test.ts
@@ -158,6 +158,18 @@ describe('http interface', function () {
     });
     strictEqual(result.response.headers['access-control-allow-origin'], 'https://docs.mongodb.com');
 
+    result = await request({
+      port: ctx.port,
+      path: `/status`,
+      headers: {
+        Origin: 'https://docs-mongodb-org-stg.s3.us-east-2.amazonaws.com',
+      },
+    });
+    strictEqual(
+      result.response.headers['access-control-allow-origin'],
+      'https://docs-mongodb-org-stg.s3.us-east-2.amazonaws.com'
+    );
+
     // Test an unparseable origin URL
     result = await request({
       port: ctx.port,
