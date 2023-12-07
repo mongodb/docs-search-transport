59ff6db75019bde9257a6cf6cf75e4bbc34b848c
Anabella Buckvar <anabellabuckvar@gmail.com>
2023-12-07 14:07:28 -0500

    DOP-4145 lint and format

--- src/SearchIndex/index.ts
+++ src/SearchIndex/index.ts
@@ -73,9 +73,9 @@ export class SearchIndex {
       return formatFacetMetaResponse(aggRes[0] as FacetAggRes, this.trieFacets);
     } catch (e) {
       log.error(
-        `Error while fetching facets for query ${query.rawQuery}, with search property ${searchProperty}, and filters ${filters} ${JSON.stringify(
-          e
-        )}`
+        `Error while fetching facets for query ${
+          query.rawQuery
+        }, with search property ${searchProperty}, and filters ${filters} ${JSON.stringify(e)}`
       );
       log.trace();
       throw e;
