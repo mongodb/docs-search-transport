b589418513f7d740dadf8d4cfe0b71c3260fe368
Seung Park <seung.park@mongodb.com>
2024-03-28 11:42:45 -0400

    test

--- src/Marian/index.ts
+++ src/Marian/index.ts
@@ -338,7 +338,7 @@ export default class Marian {
     const url = req.url || '';
 
     // call smartling API
-    const SMARTLING_URL = new URL(url, `https://mongodbdocs.sl.smartling.com/`);
+    const SMARTLING_URL = new URL(url, `mongodbdocs.sl.smartling.com`);
     // ie. https://mongodbdocs.sl.smartling.com/zh-cn/search?q=test
     try {
       const smartlingRes = await fetch(SMARTLING_URL.toString(), ogHeaders as RequestInit);
